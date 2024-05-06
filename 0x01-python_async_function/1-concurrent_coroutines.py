1223a0bab7fe300c896f4802f42a0c560c3401bf
Mohamed Kamal <ku18mz@gmail.com>
2024-05-07 02:32:13 +0300

    task 1, return sorted results for n times of calls

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,14 @@
+"""Asynchronous programming with coroutines"""
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Returns a list of all the delays (float values)
+    """
+    delays = []
+    for _ in range(n):
+        delays.append(await wait_random(max_delay))
+    return sorted(delays)
