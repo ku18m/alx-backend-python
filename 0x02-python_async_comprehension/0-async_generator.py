ec5344335195ed2decac619ffbf19b35e0df9a44
Mohamed Kamal <ku18mz@gmail.com>
2024-05-08 04:34:18 +0300

    task 0, use random*10 instead of uniform

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,4 +9,4 @@ async def async_generator() -> AsyncGenerator[float, None]:
     """Async generator that yields random numbers between 0 and 10"""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random() * 10
