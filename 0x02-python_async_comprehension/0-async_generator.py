be2b5b18e0419e4d0128ae1b76922e85711b440f
Mohamed Kamal <ku18mz@gmail.com>
2024-05-08 04:36:53 +0300

    task 0, useing generator as return annotation instead of asyncgenerator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,11 +2,11 @@
 """Async generator"""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Async generator that yields random numbers between 0 and 10"""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.random() * 10
+        yield random.uniform(0, 10)
